db2d3c14296272bd7f852706e6f9951c076a4c1b
MaksSadkov83 <maksraptor543@gmail.com>
2023-02-17 20:34:34 +0300

    Админ панель экзаменатора

---
+++ niac_test.sql
@@ -0,0 +1,283 @@
+-- MariaDB dump 10.19  Distrib 10.9.5-MariaDB, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: niac_test
+-- ------------------------------------------------------
+-- Server version	10.9.5-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `editors`
+--
+
+DROP TABLE IF EXISTS `editors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `editors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `editor_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`editor_id`,`exam_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `editors`
+--
+
+LOCK TABLES `editors` WRITE;
+/*!40000 ALTER TABLE `editors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `editors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exam_student_answers`
+--
+
+DROP TABLE IF EXISTS `exam_student_answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exam_student_answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `option_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`student_id`,`option_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam_student_answers`
+--
+
+LOCK TABLES `exam_student_answers` WRITE;
+/*!40000 ALTER TABLE `exam_student_answers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exam_student_answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exams`
+--
+
+DROP TABLE IF EXISTS `exams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text_` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exams`
+--
+
+LOCK TABLES `exams` WRITE;
+/*!40000 ALTER TABLE `exams` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `options`
+--
+
+DROP TABLE IF EXISTS `options`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `options` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL,
+  `text_` text NOT NULL,
+  `score` float NOT NULL,
+  PRIMARY KEY (`id`,`question_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `options`
+--
+
+LOCK TABLES `options` WRITE;
+/*!40000 ALTER TABLE `options` DISABLE KEYS */;
+/*!40000 ALTER TABLE `options` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question_topics`
+--
+
+DROP TABLE IF EXISTS `question_topics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question_topics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) NOT NULL,
+  `text_` text NOT NULL,
+  PRIMARY KEY (`id`,`exam_id`),
+  KEY `question_topics_fk0` (`exam_id`),
+  CONSTRAINT `question_topics_fk0` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question_topics`
+--
+
+LOCK TABLES `question_topics` WRITE;
+/*!40000 ALTER TABLE `question_topics` DISABLE KEYS */;
+/*!40000 ALTER TABLE `question_topics` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_topic_id` int(11) NOT NULL,
+  `text_` text NOT NULL,
+  PRIMARY KEY (`id`,`question_topic_id`),
+  KEY `questions_fk0` (`question_topic_id`),
+  CONSTRAINT `questions_fk0` FOREIGN KEY (`question_topic_id`) REFERENCES `question_topics` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `report_exam_students`
+--
+
+DROP TABLE IF EXISTS `report_exam_students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `report_exam_students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `exam _result` float NOT NULL,
+  `exam _date` date NOT NULL,
+  PRIMARY KEY (`id`,`exam_id`,`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `report_exam_students`
+--
+
+LOCK TABLES `report_exam_students` WRITE;
+/*!40000 ALTER TABLE `report_exam_students` DISABLE KEYS */;
+/*!40000 ALTER TABLE `report_exam_students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_users`
+--
+
+DROP TABLE IF EXISTS `role_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `text_` text NOT NULL,
+  PRIMARY KEY (`id`,`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_users`
+--
+
+LOCK TABLES `role_users` WRITE;
+/*!40000 ALTER TABLE `role_users` DISABLE KEYS */;
+INSERT INTO `role_users` VALUES
+(1,1,'SuperAdmin'),
+(2,2,'Экзаменатор');
+/*!40000 ALTER TABLE `role_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student_exams`
+--
+
+DROP TABLE IF EXISTS `student_exams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student_exams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `min_score` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`student_id`,`exam_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_exams`
+--
+
+LOCK TABLES `student_exams` WRITE;
+/*!40000 ALTER TABLE `student_exams` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student_exams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `password` text NOT NULL,
+  `telephon_number` text NOT NULL,
+  `active` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `telephon_number` (`telephon_number`) USING HASH
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES
+(1,'Super User','$2y$10$rGeguZ3gdYtL2cxZM7q6TOKhfFWToeu7EHeLSxzEupAB6Jq5.OE3e','+7 (999) 999-99-99',1),
+(2,'Экзаменатор','$2y$10$q8fLWdk4V/Qd3B7sfbEh1elNxxKNIE2Zwb8jiHoYZwvYkuPBBJ/AS','89112346534',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-17 20:30:05
